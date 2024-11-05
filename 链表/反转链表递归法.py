b3feadab1835f2d16924418e8629bf7d1c2a3c4c
king-huoye <63176037+king-huoye@users.noreply.github.com>
2024-11-05 20:45:40 +0800

    Create 反转链表递归法.py

---
+++ 链表/反转链表递归法.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        return self.reverse(head, None)
+    def reverse(self, cur: ListNode, pre: ListNode) -> ListNode:
+        if cur == None:
+            return pre
+        temp = cur.next
+        cur.next = pre
+        return self.reverse(temp, cur)
