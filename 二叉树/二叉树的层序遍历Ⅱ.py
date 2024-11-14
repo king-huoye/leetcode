3a68b119c225c062926b6b500ba105c1e7d71bb9
king-huoye <63176037+king-huoye@users.noreply.github.com>
2024-11-14 20:59:02 +0800

    Create 二叉树的层序遍历Ⅱ.py
    最后结果翻转就好

---
+++ 二叉树/二叉树的层序遍历Ⅱ.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result=[]
+        que=deque()
+        if root:
+            que.append(root)
+        while que:
+            L=len(que)
+            level=[]
+            while L:
+                L-=1
+                node=que.popleft()
+                level.append(node.val)
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+            result.append(level)
+        result[:]=result[::-1]
+        return result
+        
