c21fcb4b83f35bcfce9ede3de57d382bfcc7edc1
king-huoye <63176037+king-huoye@users.noreply.github.com>
2025-03-27 11:36:11 +0800

    Create 零钱兑换.py

---
+++ hot100/零钱兑换.py
@@ -0,0 +1,13 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n=len(coins)
+        
+        dp=[float('inf')]*(amount+1)
+        dp[0]=0
+        for i in range(len(coins)):
+            for j in range(coins[i],amount+1):
+                dp[j]=min(dp[j],dp[j-coins[i]]+1)
+        if dp[amount]==float('inf'):
+            return -1
+        else:
+            return dp[amount]
