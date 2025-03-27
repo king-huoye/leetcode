00ba8e18adc17035520847737a6e66408b7cb8f6
king-huoye <63176037+king-huoye@users.noreply.github.com>
2025-03-27 14:23:56 +0800

    Create 单词拆分.py

---
+++ hot100/单词拆分.py
@@ -0,0 +1,10 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n=len(s)
+        dp=[False]*(n+1) 
+        dp[0]=True
+        for i in range(1,n+1):
+            for j in range(0,i):
+                if s[j:i] in wordDict and dp[j]:
+                    dp[i]=True
+        return dp[n]
